52c2d28f70d80ea53abcdfe8c3f8bc3e85320b96
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-03-01 21:43:38 +0100

    Initial commit

---
+++ src/AuthService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Massfice\AuthenticatorServices;
+
+use Massfice\Service\ServiceObject;
+use Massfice\Service\ServiceData;
+
+class AuthService implements ServiceObject {
+    public $redirect;
+    public $code;
+    public $auth;
+    public $data;
+
+    public function url(array $data) : string {
+        $sid = isset($data["sid"]) ? "/?sid=".$data["sid"]->sid : "";
+        return "http://localhost/social-authenticator/auth/json".$sid;
+    }
+
+    public function prepare(&$curl, array $data) : array {
+        return [];
+    }
+
+    public function data(array $data) : ?ServiceData {
+        return null;
+    }
+
+    public function callback(int $code, array $exec) {
+        $this->auth = $exec["data"]["auth"];
+        $this->redirect = isset($exec["data"]["details"]["redirectTo"]) ? $exec["data"]["details"]["redirectTo"] : null;
+        $this->code = $code;
+        if(isset($exec["data"]["details"]["redirectTo"])) unset($exec["data"]["details"]["redirectTo"]);
+        if(isset($exec["data"]["details"]["redirectMethod"])) unset($exec["data"]["details"]["redirectMethod"]);
+        $this->data = $exec["data"]["details"];
+    }
+}
+
+?>
---
+++ src/SidService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Massfice\AuthenticatorServices;
+
+use Massfice\Service\ServiceObject;
+use Massfice\Service\ServiceData;
+
+class SidService implements ServiceObject {
+    public $sid;
+
+    public function url(array $data) : string {
+        $sid = isset($this->sid) ? "/?sid=" + $this->sid : "";
+        return "http://localhost/social-authenticator/sid/json" + $sid;
+    }
+
+    public function prepare(&$curl, array $data) : array {
+        return [];
+    }   
+
+    public function data(array $data) : ?ServiceData {
+        return null;
+    }
+
+    public function callback(int $code, array $exec) {
+        $this->sid = $exec["data"]["sid"];
+    }
+}
+
+?>
